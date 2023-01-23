9e8316f42db475d3cf3971f2dc8fc58cc7568631
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-01-22 20:55:39 -0500

    📦 NEW: add form and handlers

---
+++ src/components/Form/index.js
@@ -0,0 +1,27 @@
+import { useRef, useState } from 'react'
+
+export const Form = () => {
+  const [mail, setMail] = useState('')
+  const debounceRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('lo enviastes')
+  }
+  const handleChange = (e) => {
+    if (debounceRef.current) clearTimeout(debounceRef.current)
+
+    debounceRef.current = setTimeout(() => {
+      setMail(e.target.value)
+    }, 900)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type='email' placeholder='Correo' />
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  )
+}
