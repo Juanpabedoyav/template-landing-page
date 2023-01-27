7f1164ff159e10a6fe145f03c8f6c992f5e38385
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-01-27 17:10:33 -0500

    📦 NEW: add call to action button

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Form } from '../components/Form'
+import { CallAction } from '../components/CallAction'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Form />
+        <CallAction />
         <h1 className={styles.title}>
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
