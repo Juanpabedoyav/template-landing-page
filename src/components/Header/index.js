274392c24da0cd1e94bfd8e1cc50be51e8d5b371
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-01-27 19:23:57 -0500

    👌 IMPROVE: remove material ui and add header

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logotipo from '../../../public/assets/logotipo.png'
+
+import { HeaderSC } from './styles'
+export const Header = () => {
+  return (
+    <>
+      <HeaderSC>
+        <nav>
+          <Link className='link-item' href={'/'}>
+            <Image
+              width={120}
+              height={120}
+              alt='logo Lo Deberías Saber'
+              src={logotipo}
+            />
+          </Link>
+          <Link className='link-item' href={'/ebook'}>
+            E-Book Gratis
+          </Link>
+        </nav>
+      </HeaderSC>
+    </>
+  )
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+export const HeaderSC = styled.header`
+  width: 100%;
+  height: 14vh;
+  nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    .link-item {
+      text-decoration: none;
+      color: white;
+    }
+  }
+`
