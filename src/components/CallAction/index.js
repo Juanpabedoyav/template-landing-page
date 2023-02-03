b3ddcdf3f416c228e2db8f2a0ef8a39d7ec18637
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-02-03 18:40:26 -0500

    📦 NEW: add time countdonw

--- src/components/CallAction/index.js
+++ src/components/CallAction/index.js
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+import Countdown from 'react-countdown'
 
-export const CallAction = () => {
-  //   const [day, setDay] = useState(new Date().getDay())
-  //   const [hour, setHour] = useState(new Date().getHours())
-  //   const [minute, setMinute] = useState(new Date().getMinutes())
-  //   const [second, setSecond] = useState(new Date().getSeconds())
-
+export const CallAction = ({ time }) => {
   return (
     <>
-      {/* <section>{day + ':' + hour + ':' + minute + ':' + second}</section> */}
-      <button>Click Here</button>
+      <p
+        style={{
+          color: 'white',
+          width: '80%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          fontSize: '2rem'
+        }}
+      >
+        <span className={'title'}>El tiempo se agota</span>
+        <Countdown
+          date={Date.now() + time}
+          intervalDelay={0}
+          autoStart={true}
+          precision={3}
+        />
+      </p>
     </>
   )
 }
--- src/screens/EbookScreen/index.js
+++ src/screens/EbookScreen/index.js
@@ -2,11 +2,14 @@ import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
 import { Form } from '../../components/Form'
 import { PortadaEbook } from '../../components/PortadaEbook'
+import { CallAction } from '../../components/CallAction'
+
 export const EbookScreen = () => {
   return (
     <>
       <Header />
       <PortadaEbook />
+      <CallAction time={10000} />
       <Form text={'Descarga el E-book'} textButton={'DESCARGAR'} />
       <Footer />
     </>
